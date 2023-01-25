45a8aa6359d6edddbf6fae38d39d57975ea12c70
marcjoha <majohansson@gmail.com>
2023-01-25 14:25:39 +0100

    deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.1.1
+
+- Updated dependencies
+
 # 1.1.0
 
 - Breaking change: Renaming classes to avoid conflicts with common Flutter libs
--- test/favicon_test.dart
+++ test/favicon_test.dart
@@ -3,33 +3,32 @@ import 'package:test/test.dart';
 
 const FAVICONS = {
   'https://www.aftonbladet.se':
-      'https://www.aftonbladet.se/cnp-assets/favicon/coast-228x228.png',
+      'https://www.aftonbladet.se/cnp-assets/favicon-8447eb68/coast-228x228.png',
   'https://www.braziltravelblog.com':
       'https://www.braziltravelblog.com/wp-content/uploads/2021/04/cropped-brazil_small_flag-192x192.jpg',
-  'https://www.mashable.com': 'https://www.mashable.com/favicons/favicon.svg',
+  'https://www.mashable.com':
+      'https://www.mashable.com/favicons/android-chrome-512x512.png',
   'https://tradevenue.se/miljonarinnan30':
-      'https://tradevenue.se/sites/all/themes/tradevenue_theme/image/tradevenue_icon.png',
+      'https://cdn.32b23e.net/tradevenue/favicon.6567f653.png',
   'https://github.com/': 'https://github.githubassets.com/favicons/favicon.svg',
   'https://www.businessinsider.com/':
       'https://www.businessinsider.com/public/assets/BI/US/favicons/favicon-32x32.png',
   'https://www.mrmoneymustache.com/':
       'https://www.mrmoneymustache.com/favicon.ico',
-  'https://thedividendstory.com/':
-      'https://thedividendstory.com/wp-content/uploads/2019/02/cropped-icon_DS_transparentp-192x192.png',
+  'https://thedividendstory.blogspot.com/':
+      'https://thedividendstory.blogspot.com/favicon.ico',
   'http://dailybeast.com':
       'http://dailybeast.com/static/media/favicon.b30a79ed.ico',
-  'https://www.traveldailynews.com/':
-      'https://www.traveldailynews.com/uploads/images/setting/favicon.ico',
   'https://www.tenable.com/blog-rss':
       'https://www.tenable.com/themes/custom/tenable/img/favicons/favicon.svg',
-  'https://hbr.org/': 'https://hbr.org/favicon.ico',
+  'https://hbr.org/':
+      'https://hbr.org/resources/images/android-chrome-512x512.png',
   'http://benfrain.com/': 'http://benfrain.com/favicon2.svg',
   'http://www.alistapart.com':
       'https://i0.wp.com/alistapart.com/wp-content/uploads/2019/03/cropped-icon_navigation-laurel-512.jpg',
-  'https://fortune.com/':
-      'https://content.fortune.com/wp-content/uploads/2020/02/favicon_144.ico',
+  'https://fortune.com/': 'https://fortune.com/icons/favicons/favicon.ico',
   'https://tidochpengar.se/':
-      'https://tidochpengar.se/wp-content/uploads/2020/11/cropped-165DF740-708F-41B2-AFA0-C17794A0480E-192x192.jpeg',
+      'https://tidochpengar.b-cdn.net/wp-content/uploads/2020/11/cropped-165DF740-708F-41B2-AFA0-C17794A0480E-192x192.jpeg',
   'http://blog.tenablesecurity.com/atom.xml': null
 };
 
